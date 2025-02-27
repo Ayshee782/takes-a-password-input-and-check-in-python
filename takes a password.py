943b189b4aa766c97a75b3bea842f3554ceb0259
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-02-27 21:22:49 +0600

    Add files via upload

---
+++ takes a password.py
@@ -0,0 +1,10 @@
+"""takes a password input and checks 
+if it matches a predefined password, 
+printing "Access Granted" or "Access Denied"."""
+
+a="i@s5H88"
+b=input("enter pass= ")
+if(a==b):
+    print("Access Granted")
+else:
+    print("Access Denied")
